2dd1335d80a417558062bc95c1eab2f41bd2d3a5
Angus Lam <angushtlam@gmail.com>
2018-07-01 15:20:56 -0700

    Basic project setup

---
+++ index.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron')
+
+// Global reference to the window object.
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({height: 600, width: 800})
+  mainWindow.loadURL('http://localhost:1234/')
+
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
